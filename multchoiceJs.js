9608da80cd33d56d9997c3a5196ac32ebb2a1573
chrislepino <lepinoc@harrisoncsd.org>
2022-12-12 13:32:44 -0500

    hel

---
+++ multchoiceJs.js
@@ -0,0 +1,15 @@
+import * from "./multiplechoice.html"
+
+const nameBox = getId("nameBox")	  
+const nameButton = getId("nameBtn")
+const aLabel = getId("aLabel")
+const bLabel = getId("bLabel")
+const cLabel = getId("cLabel")
+const optionA = getId("optionA")
+const optionB = getId("optionB")
+const optionC = getId("optionC")
+const nextButton = getId("nextButton")
+const question = getId("question")
+var questionNumber = 0
+var numRight = 0
+var numWrong = 0// JavaScript Document
